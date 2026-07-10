c049fec0ad964a0c7da3ce279f80ace6e10f09d1
Mikri Akrida <zepamonha711@gmail.com>
2026-07-10 18:17:16 -0300

    Delete Main.java
    Class moved to the system package.

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        GameHeart etapas = new GameHeart();
-        etapas.initialCheck();
-        etapas.gameStart();
-        etapas.countDownAndSunDetails();
-    }
-}
