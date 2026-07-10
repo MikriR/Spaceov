3983cbf446d06d6d9ed1bd4d19883d4d54333bb4
Mikri Akrida <zepamonha711@gmail.com>
2026-07-10 18:12:27 -0300

    Packages increment
    I'm just keeping code better organized.

---
+++ system/Main.java
@@ -0,0 +1,11 @@
+package br.com.system;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GameHeart steps = new GameHeart();
+        steps.initialCheck();
+        steps.gameStart();
+        steps.countDownAndSunDetails();
+    }
+}
