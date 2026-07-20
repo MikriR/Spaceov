d3af27b0b2c263e1af2dd2a69261f0cc72737bf5
Mikri Akrida <zepamonha711@gmail.com>
2026-07-20 17:46:43 -0300

    Evitando erros de compilação
    Adicionando as pastas br e com que fazem parte das pastas dos pacotes das classes.

---
+++ src/br/com/several/Universe.java
@@ -0,0 +1,15 @@
+package br.com.several;
+
+import java.util.ArrayList;
+
+public class Universe {
+
+    private static ArrayList<String> destinos = new ArrayList<>();
+
+    public static ArrayList<String> getDestinos() {
+        destinos.add("SOL");
+        return destinos;
+    }
+}
+
+
